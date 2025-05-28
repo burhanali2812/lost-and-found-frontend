3a841a8b35a946b290108784baea7474e39924af
burhanali2812 <syedburhanali830@gmail.com>
2025-05-28 16:25:55 +0500

    update modal

--- src/components/UserVerification.js
+++ src/components/UserVerification.js
@@ -252,7 +252,8 @@ function UserVerification({ user, setUser }) {
         aria-labelledby="verifyUserLabel"
         aria-hidden="true"
       >
-        <div className="modal-dialog modal-lg">
+        <div className="modal-dialog modal-dialog-centered">
+
           <div className="modal-content bg-dark text-light rounded-4 shadow">
             <div className="modal-header border-0">
               <h5 className="modal-title" id="verifyUserLabel">
